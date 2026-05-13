29dc8a9c801e065b0c251da4db90f2c55606ee94
sudo-melanin <amosemma93@gmail.com>
2026-05-13 15:27:51 +0100

    chore: format code

--- lib/features/workspace/presentation/presentation.dart
+++ lib/features/workspace/presentation/presentation.dart
@@ -2,4 +2,4 @@ export 'views/views.dart';
 export 'components/components.dart';
 export 'providers/providers.dart';
 export 'providers/workspace_notifier.dart';
-export 'providers/workspace_state.dart';  
+export 'providers/workspace_state.dart';
